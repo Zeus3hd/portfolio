fa6f24e3a2f668d71755721df3c30c8f4b485c0b
Ahd <dafuq.ahd@gmail.com>
2020-05-30 18:39:09 +0300

    extract bottom icons in their own components and install react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import WelcomeScreen from "./components/WelcomeScreen";
+import BottomButtons from "./components/BottomButtons";
 import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <WelcomeScreen />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={WelcomeScreen} />
+        </Switch>
+        <BottomButtons />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/BottomButtons/index.style.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+const colors = {
+  white: "#ffffff",
+  red: "#FF0000",
+  green: "#05FF00",
+};
+const IconsWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+const IconButton = styled.button`
+  background: transparent;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  svg {
+    fill: ${colors.white};
+    width: 30px;
+  }
+  > p {
+    color: ${colors.white};
+    font-family: "Press Start 2p";
+    padding: 0.5rem 0;
+    font-size: 0.6rem;
+  }
+`;
+
+export { IconsWrapper, IconButton };
