308892ce99a12de937d7557d82da6af24e696c3a
Ahd <dafuq.ahd@gmail.com>
2020-05-31 21:18:55 +0300

    create skills page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,10 +1,70 @@
 import React from "react";
-import { Wrapper } from "./index.style";
+import {
+  ProjectsLink,
+  Wrapper,
+  ListWrapper,
+  HeadLine,
+  List,
+  ListItem,
+  BackLink,
+} from "./index.style";
 import ScoreBar from "../ScoreBar";
 const Skills = () => {
   return (
     <Wrapper>
       <ScoreBar world="2-1" />
+      <HeadLine>Web Development:</HeadLine>
+      <ListWrapper>
+        <List>
+          <ListItem>JavaScript</ListItem>
+          <ListItem>ReactJS</ListItem>
+          <ListItem>NodeJS</ListItem>
+          <ListItem>HTML/CSS/SASS</ListItem>
+          <ListItem>ExpressJS</ListItem>
+        </List>
+        <List>
+          <ListItem>Python</ListItem>
+          <ListItem>Flask</ListItem>
+          <ListItem>PHP (Beginner)</ListItem>
+          <ListItem>WordPress</ListItem>
+          <ListItem>Styled-Components</ListItem>
+        </List>
+      </ListWrapper>
+
+      <HeadLine>Headless CMS & SDKs:</HeadLine>
+      <ListWrapper>
+        <List>
+          <ListItem>Headless WordPress</ListItem>
+          <ListItem>Firebase SDK</ListItem>
+          <ListItem>Contentful SDK</ListItem>
+          <ListItem>Strapi</ListItem>
+        </List>
+      </ListWrapper>
+
+      <HeadLine>Mobile Development:</HeadLine>
+      <ListWrapper>
+        <List>
+          <ListItem>Flutter / Dart</ListItem>
+          <ListItem>React Native (intermediate)</ListItem>
+        </List>
+      </ListWrapper>
+      <HeadLine>Photo Editing & Prototyping:</HeadLine>
+      <ListWrapper>
+        <List>
+          <ListItem>Adobe Photoshop</ListItem>
+          <ListItem>Gimp</ListItem>
+          <ListItem>Adone xD</ListItem>
+        </List>
+        <List>
+          <ListItem>Figma</ListItem>
+          <ListItem>InkScape</ListItem>
+        </List>
+      </ListWrapper>
+
+      <ProjectsLink href="#deadsies">
+        Press Here to Conquer Projects
+      </ProjectsLink>
+      <BackLink href="#deadsies">Go Back</BackLink>
     </Wrapper>
   );
 };
--- src/components/Skills/index.style.js
+++ src/components/Skills/index.style.js
@@ -4,8 +4,68 @@ const colors = {
   white: "#ffffff",
   red: "#FF0000",
   green: "#05FF00",
+  yellow: "#FFF500",
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+`;
 
-export { Wrapper };
+const HeadLine = styled.h2`
+  color: ${colors.green};
+  font-size: 0.7rem;
+  padding: 0 0.4rem;
+  box-sizing: border-box;
+  letter-spacing: 0.15rem;
+  line-height: 1.2rem;
+`;
+const ListWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const List = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0 0.5rem;
+`;
+const ListItem = styled.li`
+  position: relative;
+  padding-left: 1.3rem;
+  color: ${colors.white};
+  font-size: 0.6rem;
+  line-height: 1.1rem;
+  &:before {
+    content: "- ";
+    position: absolute;
+    left: 0;
+  }
+`;
+
+const ProjectsLink = styled.a`
+  font-size: 0.7rem;
+  text-align: center;
+  margin: 1.5rem 0;
+  color: ${colors.yellow};
+  text-decoration: none;
+`;
+
+const BackLink = styled.a`
+  font-size: 0.7rem;
+  text-align: center;
+  margin: 1.5rem 0;
+  color: ${colors.white};
+  text-decoration: none;
+`;
+
+export {
+  BackLink,
+  ProjectsLink,
+  Wrapper,
+  HeadLine,
+  ListWrapper,
+  List,
+  ListItem,
+};
